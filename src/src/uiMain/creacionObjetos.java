14e7b8805f09d7b9db895dec6af3440dd9d122e4
espinosasteban <esespinosap@unal.edu.co>
2022-10-13 20:40:44 -0500

    Eliminación de código (proyecto previo)

--- src/src/uiMain/creacionObjetos.java
+++ src/src/uiMain/creacionObjetos.java
@@ -1,11 +1,9 @@
 package uiMain;
-import gestorAplicacion.paquete1.*;
-import gestorAplicacion.paquete2.*;
+import gestorAplicacion.libreria.*;
+import gestorAplicacion.servicios.*;
 
 import java.util.ArrayList;
 
 public class creacionObjetos {
-    Estudiante est1 = new Estudiante("Camilo",27893,15, 10,"Mañana",new Matricula(),
-            true,new ArrayList<Materia>(),new Salon(),3.4f);
 
 }
