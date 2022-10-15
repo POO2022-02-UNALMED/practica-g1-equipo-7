4e5ebf1dc426c188998424da3990b9e17bce9ec7
espinosasteban <esespinosap@unal.edu.co>
2022-10-15 15:16:49 -0500

    Creación de constructores Revista
    Creé otros dos constructores que hacen uso del this() para que cuando se cree un objeto Revista sin parámetros, o sólo con categoría, sus parámetros no queden en null

--- src/src/gestorAplicacion/libreria/Revista.java
+++ src/src/gestorAplicacion/libreria/Revista.java
@@ -7,11 +7,20 @@ public class Revista extends Titulo{
     public static ArrayList<EjemplarRevista> ejemplares = new ArrayList<>();
 
     public Revista(String categoria) {
-        this.categoria = categoria;
+        this("Independiente","Escritor anónimo",(int) (Math.random() * 10000), categoria);
+
+
+
+
     }
 
     public Revista(String nombre, String autor, int ISBN, String categoria) {
         super(nombre, autor, ISBN);
         this.categoria = categoria;
+    }
+
+    public Revista() {
+        this("Revista UNAL","Universidad Nacional de Colombia", (int) (Math.random() * 10000),"Actualidad");
+
     }
 }
