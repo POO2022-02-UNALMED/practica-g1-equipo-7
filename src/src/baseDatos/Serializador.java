4879dc2e38dbaf44372519123c9820b5639c19cd
SamuelMezaT <smezat@unal.edu.co>
2022-10-22 15:51:24 -0500

    serealizador in baseDatos

---
+++ src/src/baseDatos/Serializador.java
@@ -0,0 +1,84 @@
+package baseDatos;
+
+import gestorAplicacion.libreria.Biblioteca;
+
+import java.io.*;
+
+public class Serializador {
+    private static File rutaTemp = new File("src\\src\\baseDatos\\temp");
+
+    /**
+     * Metodo para serialzar las listas que están en la clase Biblioteca
+     */
+    public static void serializar(Biblioteca biblioteca) {
+        FileOutputStream fos;
+        ObjectOutputStream oos;
+        File[] docs = rutaTemp.listFiles();
+        PrintWriter pw;
+
+        //Con el for se borra el contenido de los archivos para evitar futuros problemas
+        for (File file : docs) {
+            try {
+                //al pasarle como parametro al pw, la ruta del archivo, borra el contenido
+                pw = new PrintWriter(file);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        //Se escribe lo que debe ir en cada archivo segun lo que contiene biblioteca
+        for (File file : docs) {
+            if (file.getAbsolutePath().contains("ejemplaresLibros")) {
+                try {
+                    fos = new FileOutputStream(file);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(biblioteca.getEjemplaresLibros());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("ejemplaresRevistas")) {
+                try {
+                    fos = new FileOutputStream(file);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(biblioteca.getEjemplaresRevistas());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("libros")) {
+                try {
+                    fos = new FileOutputStream(file);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(biblioteca.getLibros());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("revistas")) {
+                try {
+                    fos = new FileOutputStream(file);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(biblioteca.getRevistas());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else if (file.getAbsolutePath().contains("usuarios")) {
+                try {
+                    fos = new FileOutputStream(file);
+                    oos = new ObjectOutputStream(fos);
+                    oos.writeObject(biblioteca.getUsuario());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
